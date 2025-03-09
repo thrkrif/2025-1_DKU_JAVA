834036481857efc82b5145d31f28e746bb9bea9b
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-03-09 19:03:59 +0900

    스프링 설정

---
+++ 1week/Lab0_32207522_양상훈/spring-boot-hello/src/test/java/kr/ac/dankook/ace/spring_boot_hello/SpringBootHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.dankook.ace.spring_boot_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
